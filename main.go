eea090faf40a7bf490b1478bd4cb1f0dc1987eb2
toppev <44984598+toppev@users.noreply.github.com>
2021-05-20 22:46:16 +0300

    Progress, needs testing

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"encoding/json"
+	"github.com/joho/godotenv"
 	"helm.sh/helm/v3/pkg/action"
 	"helm.sh/helm/v3/pkg/chart/loader"
 	"helm.sh/helm/v3/pkg/cli"
@@ -10,54 +12,72 @@ import (
 	"os"
 )
 
-type server struct{}
-
-func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"message": "hello world"}`))
-}
+var (
+	chartPath string
+	namespace string
+)
 
 func main() {
-	s := &server{}
-	http.Handle("/", s)
+	log.Println("Starting server...")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file", err)
+	}
+	chartPath = os.Getenv("CHART_PATH")
+	namespace = os.Getenv("KUBE_NAMESPACE")
+	log.Printf("chartPath: %s", chartPath)
+	log.Printf("namespace: %s", namespace)
+	http.HandleFunc("/start-chart", startChart)
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
-// Some help here https://stackoverflow.com/questions/45692719/samples-on-kubernetes-helm-golang-client
-func installChart() {
-	chartPath := "/some/path"
-	namespace := "default"
-	releaseName := "my-release"
+type RequestBody struct {
+	releaseName string
+	values      map[string]interface{}
+}
 
-	// Test/example values (TODO: from request)
-	values := map[string]interface{}{
-		"example": map[string]interface{}{
-			"addr": "localhost",
-			"port": "26379",
-		},
+func startChart(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	if r.Method != http.MethodPost {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	} else {
+		r.Body = http.MaxBytesReader(w, r.Body, 1024*1024) // max 2 MB
+		dec := json.NewDecoder(r.Body)
+		dec.DisallowUnknownFields()
+		var body RequestBody
+		err := dec.Decode(&body)
+		if err != nil {
+			msg := "Failed to parse request body"
+			log.Printf("Request failed: %s (%s)", err.Error(), msg)
+			http.Error(w, msg, http.StatusBadRequest)
+			return
+		}
+		upgradeOrInstallChart(body.releaseName, body.values)
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"message": "success"}`))
 	}
+}
 
+// Some help here https://stackoverflow.com/questions/45692719/samples-on-kubernetes-helm-golang-client
+func upgradeOrInstallChart(releaseName string, values map[string]interface{}) {
 	// Load the chart
 	chart, err := loader.Load(chartPath)
 	if err != nil {
 		panic(err)
 	}
-
 	actionConf := new(action.Configuration)
 	clientGetter := cli.New().RESTClientGetter()
 	if err := actionConf.Init(clientGetter, namespace, os.Getenv("HELM_DRIVER"), log.Printf); err != nil {
 		log.Printf("%+v", err)
 	}
 
-	iCli := action.NewInstall(actionConf)
-	iCli.Namespace = namespace
-	iCli.ReleaseName = releaseName
+	client := action.NewUpgrade(actionConf)
+	client.Namespace = namespace
 	// client.DryRun = true // - for testing
 
-	rel, err := iCli.Run(chart, values)
+	rel, err := client.Run(releaseName, chart, values)
 	if err != nil {
 		panic(err)
 	}
-	log.Printf("Installed Chart from path: %s in namespace: %s\n", rel.Name, rel.Namespace)
+	log.Printf("Installed/Upgraded Chart '%s' from path: '%s' in namespace: '%s'\n", releaseName, rel.Name, rel.Namespace)
 }
